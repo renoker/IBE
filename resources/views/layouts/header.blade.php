7a3d90a0bd72a7539728c6f8cd34674a25ce55fd
renoker <rodolfoulises.ramirez@gmail.com>
2024-03-18 13:49:33 -0600

    Header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,41 @@
+<header class="header">
+    <div class="header_box">
+        <ul>
+            <li class="active">
+                <a href="" class="menu">
+                    Nosotros
+                </a>
+            </li>
+            <li>
+                <a href="" class="menu">
+                    Turnkey
+                </a>
+            </li>
+            <li>
+                <a href="" class="menu">
+                    Catálogo
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <div class="logo"></div>
+                </a>
+            </li>
+            <li>
+                <a href="" class="menu">
+                    Servicios
+                </a>
+            </li>
+            <li>
+                <a href="" class="menu">
+                    Contacto
+                </a>
+            </li>
+            <li>
+                <a href="" class="menu">
+                    Redes
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+@section('title', 'IBE - Home')
+
+@section('content')
+@endsection
