2ecee49a3e7a1c433fee72add2cc8bbfcde4b5ed
renoker <rodolfoulises.ramirez@gmail.com>
2024-03-18 16:15:20 -0600

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,131 @@
+<footer>
+    <div class="part_one">
+        <div class="footer-top">
+            <div class="part-left">
+                <div class="box_info_footer">
+                    <div class="left">
+                        <img src="{{ url('icons/IBE-Logotipo_06.png') }}" alt="" class="logo_footer">
+                        <h6 class="h6_logo">Integral Solutions</h6>
+                        <p class="p_logo">Procesos Productivos <br> Agroindustriales</p>
 
+                        <p class="info_left_footer">
+                            Es un placer para nosotros ponernos a sus <br> órdenes, y presentarle toda la gama de <br>
+                            servicios y productos con las que cuenta <br> IntegraIBE
+                        </p>
+                    </div>
+                    <div class="right">
+                        <div class="item">
+                            <img src="{{ url('icons/tel.png') }}" class="icon" alt="">
+                            <div class="list">
+                                <p>452-524-6479</p>
+                                <p>452-340-5913</p>
+                                <p>452-182-0611 </p>
+                            </div>
+                        </div>
+                        <div class="item-email">
+                            <img src="{{ url('icons/email.png') }}" class="icon" alt="">
+                            <div class="list">
+                                <p>ventasonline.ibe@gmail.com</p>
+                                <p>ventasonlineibe@integraibe.com.mx</p>
+                            </div>
+                        </div>
+                        <div class="item-home">
+                            <img src="{{ url('icons/email.png') }}" class="icon" alt="">
+                            <div class="list">
+                                <p>Héroes de Nacozari #1 <br>
+                                    Col. Eduardo Ruiz <br>
+                                    C.P.60130, Uruapan <br>
+                                    Michoacán</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="parte-right">
+                <div class="box">
+                    <h5 class="encabezado">Contáctanos</h5>
+                    <p class="p">En IBE Solutions queremos ser un auxiliar primordial para el desarrollo de su
+                        empresa. compártenos tus datos de contacto</p>
+                    <form action="" method="post">
+                        <div class="box_input">
+                            <div class="icon_input">
+                                <img src="{{ url('icons/user.png') }}" alt="">
+                            </div>
+                            <input type="text" placeholder="Compañia">
+                        </div>
+                        <div class="box_input mt-15">
+                            <div class="icon_input">
+                                <img src="{{ url('icons/cartita.png') }}" alt="">
+                            </div>
+                            <input type="text" placeholder="Correo">
+                        </div>
+                        <div class="content_button_footer_send">
+                            <button type="submit">Envíar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="part_two">
+        <div class="footer-bottom">
+            <ul>
+                <li>
+                    <a href="" class="menu">
+                        Nosotros
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="menu">
+                        Turnkey
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="menu">
+                        Catálogo
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="menu">
+                        Contacto
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="menu">
+                        Servicio
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="menu">
+                        Politica de privacidad
+                    </a>
+                </li>
+            </ul>
+            <div class="redes_all">
+                <div class="redes">
+                    <a href="">
+                        <img src="{{ url('icons/Linkedin.png') }}" alt="">
+                    </a>
+                    <a href="">
+                        <img src="{{ url('icons/Instagram.png') }}" alt="">
+                    </a>
+                    <a href="">
+                        <img src="{{ url('icons/Behance.png') }}" alt="">
+                    </a>
+                    <a href="">
+                        <img src="{{ url('icons/Tiktok.png') }}" alt="">
+                    </a>
+                    <a href="">
+                        <img src="{{ url('icons/Youtube.png') }}" alt="">
+                    </a>
+                    <a href="">
+                        <img src="{{ url('icons/Facebook.png') }}" alt="">
+                    </a>
+                </div>
+                <div class="derechos">
+                    <p class="dre">© 2023, All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
